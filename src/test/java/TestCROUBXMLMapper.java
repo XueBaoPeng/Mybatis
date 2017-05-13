3645b81b5f64935df756859038448e5cc3a5bbe3
xbp <13519297683@163.com>
2017-05-13 17:40:53 +0800

    用注解方式执行Mybatis的sql语句和用xml配置文件执行sql

--- src/test/java/TestCROUBXMLMapper.java
+++ src/test/java/TestCROUBXMLMapper.java
@@ -3,43 +3,84 @@ import org.testng.annotations.Test;
 import xbp.mybatis.domain.User;
 import xbp.mybatis.utils.MyBatisUtils;
 
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/5/13 0013.
  */
 public class TestCROUBXMLMapper {
 
-
     @Test
     public void testAdd() {
+        //SqlSession sqlSession = MyBatisUtil.getSqlSession(false);
         SqlSession sqlSession = MyBatisUtils.getSqlSession(true);
-/**
- * 映射sql的标识字符串，
- * me.gacl.mapping.userMapper是userMapper.xml文件中mapper标签的namespace属性的值，
- * addUser是insert标签的id属性值，通过insert标签的id属性值就可以找到要执行的SQL
- */
-        String statement = "xbp.mybatis.domain.User.addUser";
+        /**
+         * 映射sql的标识字符串，
+         * me.gacl.mapping.userMapper是userMapper.xml文件中mapper标签的namespace属性的值，
+         * addUser是insert标签的id属性值，通过insert标签的id属性值就可以找到要执行的SQL
+         */
+        String statement = "xbp.mybatis.domain.User.addUser";//映射sql的标识字符串
         User user = new User();
-        user.setName("孤芳自赏");
-        user.setAge(33);
-        int retResult = sqlSession.insert(statement,user);
+        user.setName("用户孤傲苍狼");
+        user.setAge(20);
+        //执行插入操作
+        int retResult = sqlSession.insert(statement, user);
+        //手动提交事务
+        //sqlSession.commit();
+        //使用SqlSession执行完SQL之后需要关闭SqlSession
         sqlSession.close();
-        System.out.print(retResult);
-
+        System.out.println(retResult);
     }
 
     @Test
-    public void testUpdate(){
-        SqlSession sqlSession=MyBatisUtils.getSqlSession(true);
-
-        String statement = "xbp.mybatis.domain.User.updateUser";
-        User user=new User();
+    public void testUpdate() {
+        SqlSession sqlSession = MyBatisUtils.getSqlSession(true);
+        /**
+         * 映射sql的标识字符串，
+         * me.gacl.mapping.userMapper是userMapper.xml文件中mapper标签的namespace属性的值，
+         * updateUser是update标签的id属性值，通过update标签的id属性值就可以找到要执行的SQL
+         */
+        String statement = "xbp.mybatis.domain.User.updateUser";//映射sql的标识字符串
+        User user = new User();
         user.setId(3);
-        user.setAge(22);
-        user.setName("mmeids");
-
-        int retResult=sqlSession.update(statement,user);
+        user.setName("孤傲苍狼");
+        user.setAge(25);
+        //执行修改操作
+        int retResult = sqlSession.update(statement, user);
+        //使用SqlSession执行完SQL之后需要关闭SqlSession
         sqlSession.close();
-        System.out.print(retResult);
+        System.out.println(retResult);
     }
 
+    @Test
+    public void testDelete() {
+        SqlSession sqlSession = MyBatisUtils.getSqlSession(true);
+        /**
+         * 映射sql的标识字符串，
+         * me.gacl.mapping.userMapper是userMapper.xml文件中mapper标签的namespace属性的值，
+         * deleteUser是delete标签的id属性值，通过delete标签的id属性值就可以找到要执行的SQL
+         */
+        String statement = "xbp.mybatis.domain.User.deleteUser";//映射sql的标识字符串
+        //执行删除操作
+        int retResult = sqlSession.delete(statement, 5);
+        //使用SqlSession执行完SQL之后需要关闭SqlSession
+        sqlSession.close();
+        System.out.println(retResult);
+    }
+
+    @Test
+    public void testGetAll() {
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+        /**
+         * 映射sql的标识字符串，
+         * me.gacl.mapping.userMapper是userMapper.xml文件中mapper标签的namespace属性的值，
+         * getAllUsers是select标签的id属性值，通过select标签的id属性值就可以找到要执行的SQL
+         */
+        String statement = "xbp.mybatis.domain.User.getAllUsers";//映射sql的标识字符串
+        //执行查询操作，将查询结果自动封装成List<User>返回
+        List<User> lstUsers = sqlSession.selectList(statement);
+        //使用SqlSession执行完SQL之后需要关闭SqlSession
+        sqlSession.close();
+        System.out.println(lstUsers);
+    }
 }
